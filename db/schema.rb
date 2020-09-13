638005e5dfc5d0b1dbb726628552b942b03805f3
Hayato Clarke <hayatoclarke@gmail.com>
2020-09-13 15:19:50 +0900

    coin amount changed to value

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_134908) do
+ActiveRecord::Schema.define(version: 2020_09_13_061828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "coins", force: :cascade do |t|
     t.string "currency"
-    t.decimal "amount"
+    t.decimal "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "jar_id", null: false
